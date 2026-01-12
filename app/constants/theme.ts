2406c8016c03698c742835f8ee13af591b1f819d
agustinasanguinetti89-cmyk <agustina.sanguinetti89@gmail.com>
2026-01-12 16:18:05 +0100

    Create theme constants file

---
+++ app/constants/theme.ts
@@ -0,0 +1,13 @@
+export const THEME = {
+  colors: {
+    primary: '#751f82',
+    dark: '#3a2459',
+    light: '#f6f3f7',
+    white: '#ffffff',
+    text: '#2a2a2a',
+  },
+  fonts: {
+    title: 'Montserrat',
+    body: 'Inter',
+  },
+};
